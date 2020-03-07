bf1d28cf24e7a94dc66bce897af9114f43d15b53
Ross Miglin <rossmiglin@gmail.com>
2020-03-07 02:20:21 -0500

    succesfully created fractal maker under "Projects" tab

---
+++ projects/fractals.js
@@ -0,0 +1,44 @@
+function drawFractal(){
+  var myCanvas = document.createElement("canvas")
+  myCanvas.width = screen.width;
+  myCanvas.height = screen.height;
+  document.body.appendChild(myCanvas);
+  var ctx = myCanvas.getContext("2d");
+
+  var magnificationFactor = 3000;
+  var panX = -0.15;
+  var panY = 0.5;
+  for(var x = 0; x < myCanvas.width; x++) {
+    for(var y = 0; y < myCanvas.height; y++) {
+      var belongsToSet = checkIfBelongsToMandelbrotSet(x/magnificationFactor - panX,
+                                                        y/magnificationFactor - panY);
+      if(belongsToSet == 0) {
+        ctx.fillStyle = '#000';
+        ctx.fillRect(x,y,1,1);
+      } else {
+        ctx.fillStyle = 'hsl(0, 100%, ' + belongsToSet + '%)';
+        ctx.fillRect(x,y,1,1);
+      }
+    }
+  }
+}
+drawFractal()
+
+function checkIfBelongsToMandelbrotSet(x, y){
+  var realComponentOfResult = x;
+  var imaginaryComponentOfResult = y;
+  var maxIterations = 100;
+  for(var i = 0; i < 10; i++){
+    var tempRealComponent = realComponentOfResult * realComponentOfResult
+                            - imaginaryComponentOfResult * imaginaryComponentOfResult
+                            + x;
+    var tempImaginaryComponent = 2 * realComponentOfResult * imaginaryComponentOfResult
+                            + y;
+    realComponentOfResult = tempRealComponent;
+    imaginaryComponentOfResult = tempImaginaryComponent;
+  if(realComponentOfResult * imaginaryComponentOfResult > 5)
+    return (i/maxIterations * 100);
+  }
+  return 0;
+
+}
